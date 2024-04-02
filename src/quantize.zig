e712e373b579912ea83398a36d2666702a40746e
injuly <srijannr1@gmail.com>
2024-04-02 16:11:13 +0530

    add untested median cut quantization

--- src/quantize.zig
+++ src/quantize.zig
@@ -33,7 +33,7 @@ const QuantizedColor = struct {
     RGB: [3]u8,
     /// Frequency of the color in the original image.
     frequency: usize,
-    /// Index into the color subdivion array.
+    /// Index into the color-space partitions array.
     new_index: u8,
     /// Next color in the linked list.
     next: ?*QuantizedColor,
@@ -41,6 +41,8 @@ const QuantizedColor = struct {
 
 /// A subdivison of the color space produced by the median cut algorithm.
 const ColorSpace = struct {
+    const Self = @This();
+
     /// The color channel in this partition with the highest range
     widest_channel: Channel,
     // width of the widest channel in this partition.
@@ -55,6 +57,29 @@ const ColorSpace = struct {
     num_colors: usize,
     /// Number of pixels that this partition accounts for.
     num_pixels: usize,
+
+    /// Find the average of all the colors in this partition.
+    fn averageColor(self: *const Self) [3]u8 {
+        var rgb = [3]u32{ 0, 0, 0 };
+        var color = self.colors;
+        std.debug.assert(color != null);
+        for (0..self.num_colors) |_| {
+            if (color.next) |c| {
+                color = c;
+                for (0..3) |j| {
+                    rgb[j] += color.RGB[j] * color.frequency;
+                }
+            } else {
+                break;
+            }
+        }
+
+        for (0..3) |i| {
+            rgb[i] /= self.num_pixels;
+        }
+
+        return rgb;
+    }
 };
 
 const Channel = enum(u5) { Red = 0, Blue = 1, Green = 2 };
@@ -78,16 +103,6 @@ test "pixel comparison function" {
     try std.testing.expectEqual(false, colorLessThan(Channel.Blue, &a, &b));
 }
 
-const Pixel = packed struct {
-    r: u8,
-    g: u8,
-    b: u8,
-    _: u8 = undefined, // padding to fit in u32
-    comptime {
-        std.debug.assert(@sizeOf(Pixel) == @sizeOf(u32));
-    }
-};
-
 fn colorFromRGB(r: u8, g: u8, b: u8) QuantizedColor {
     return QuantizedColor{
         .RGB = [3]u8{ r, g, b },
@@ -109,12 +124,12 @@ test "sorting pixels by color channel" {
 
     std.sort.heap(*QuantizedColor, &input, Channel.Red, colorLessThan);
     for (0..input.len) |i| {
-        try std.testing.expectEqual(want[i].RGB, input[i].RGB);
+        try std.testing.expectEqual(want[i], input[i]);
     }
 }
 
-const bits_per_prim_color = 5;
-const max_prim_color = (2 ** bits_per_prim_color) - 1;
+const bits_per_prim_color = 5; // 5 bits per color channel.
+const max_prim_color = (2 ** bits_per_prim_color) - 1; // 0x1f
 const shift = 8 - bits_per_prim_color;
 
 const RGB5 = struct {
@@ -135,12 +150,12 @@ pub fn quantize(allocator: std.mem.Allocator, rgb_buf: []u8) !QuantizeResult {
     const all_colors = try allocator.alloc(QuantizedColor, color_array_size);
     for (0.., all_colors) |i, *color| {
         color.frequency = 0;
-        // The RGB values are a color stored in the lower 15 bits of its index
+        color.new_index = 0;
+        // The RGB values are packed in the lower 15 bits of its index
         // 0x--(RRRRR)(GGGGG)(BBBBB)
         color.RGB[0] = i >> (2 * bits_per_prim_color); // R: upper 5 bits
         color.RGB[1] = (i >> bits_per_prim_color) & max_prim_color; // G: middle 5 bits
         color.RGB[2] = i & max_prim_color; // B: lower 5 bits.
-        color.new_index = i;
     }
 
     // Sample all colors in the image, and count their frequency.
@@ -163,20 +178,20 @@ pub fn quantize(allocator: std.mem.Allocator, rgb_buf: []u8) !QuantizeResult {
     }
 
     // Find all colors in the color table that are used at least once, and chain them.
-    var head: *QuantizedColor = null;
+    var head: *QuantizedColor = undefined;
     for (all_colors) |*color| {
         if (color.frequency > 0) {
             head = color;
             break;
         }
     }
-    std.debug.assert(head != null);
+
+    std.debug.assert(head.frequency > 0 and head.new_index == 0);
 
     var qcolor = head;
     var color_count: usize = 0;
-    // minimum/maximum values of R,G, and B in this partition respectively
     for (all_colors) |*color| {
-        if (color.frequency > 0) {
+        if (color != head and color.frequency > 0) {
             qcolor.next = color;
             qcolor = color;
             color_count += 1;
@@ -184,19 +199,63 @@ pub fn quantize(allocator: std.mem.Allocator, rgb_buf: []u8) !QuantizeResult {
     }
     qcolor.next = null;
 
-    const partitions = try allocator.alloc(ColorSpace, 2 ** 3);
-    defer allocator.free(partitions);
-    for (partitions) |*partition| {
-        partition.colors = null;
-        partition.num_colors = 0;
+    var first_partition = try allocator.create(ColorSpace);
+    first_partition.colors = head;
+    first_partition.num_colors = color_count;
+    first_partition.num_pixels = n_pixels;
+
+    findWidestChannel(&first_partition);
+
+    const partitions = try medianCut(
+        allocator,
+        all_colors,
+        first_partition,
+        3,
+    );
+
+    defer {
+        for (partitions) |p| {
+            allocator.destroy(p);
+        }
+        allocator.free(partitions);
     }
 
-    var first_partition = &partitions[0];
-    first_partition.head = head;
-    first_partition.num_colors = color_count;
-    first_partition.widest_channel = findWidestChannel(head);
-    first_partition.num_pixels = n_pixels;
-    try medianCut(allocator, all_colors, &partitions[0], 3);
+    const color_table = try allocator.alloc(u8, partitions.len * 3);
+    for (0.., partitions) |i, partition| {
+        // 1. find the average color of this partition.
+        const avg_color = partition.averageColor();
+        color_table[i * 3] = avg_color;
+        color_table[i * 3 + 1] = avg_color[1];
+        color_table[i * 3 + 2] = avg_color[2];
+
+        // 2. Point all colors in this partition to the index of this partition.
+        var color = partition.colors;
+        for (0..partition.num_colors) |_| {
+            color.new_index = i;
+            if (color.next) |next| {
+                color = next;
+            } else {
+                unreachable;
+            }
+        }
+    }
+
+    // Now go over the input image, and replace each pixel with the index of the partition
+    var image_buf = try allocator.alloc(u8, n_pixels);
+    for (0..n_pixels) |i| {
+        const r = rgb_buf[i * 3];
+        const g = rgb_buf[i * 3 + 1];
+        const b = rgb_buf[i * 3 + 2];
+
+        const r_mask = @as(usize, r >> shift) << (2 * bits_per_prim_color);
+        const g_mask = @as(usize, g >> shift) << bits_per_prim_color;
+        const b_mask = @as(usize, b >> shift);
+        const index: usize = r_mask | g_mask | b_mask;
+
+        image_buf[i] = all_colors[index].new_index;
+    }
+
+    return QuantizeResult.init(color_table, image_buf);
 }
 
 /// Find the color channel with the largest range in the given parition.
@@ -266,104 +325,136 @@ test "findWidestChannel" {
     try std.testing.expectEqual(.Green, colorspace.widest_channel);
 }
 
-fn medianCut(allocator: std.mem.Allocator, first_partition: *ColorSpace, depth: u3) !void {
+/// Copy all the colors in a partition into an array,
+/// then sort that array along the widest channel of the partition, and return it.
+/// The array contains pointers to the original colors in the partition.
+/// The array is owned by the caller, and must be kept alive at least as long as the partition itself.
+fn sortPartition(allocator: std.mem.Allocator, partition: *const ColorSpace) ![]QuantizedColor {
+    var sorted_colors = try allocator.alloc(*QuantizedColor, partition.num_colors);
+    var color = partition.colors;
+    for (0..partition.num_colors) |i| {
+        std.debug.assert(color != null);
+        sorted_colors[i] = color;
+        color = color.next;
+    }
+
+    std.sort.heap(
+        *QuantizedColor,
+        sorted_colors,
+        partition.widest_channel,
+        colorLessThan,
+    );
+
+    for (0..sorted_colors.len - 1) |i| {
+        sorted_colors[i].next = sorted_colors[i + 1];
+    }
+    sorted_colors[sorted_colors.len - 1].next = null;
+    return sorted_colors;
+}
+
+/// Given a list of partitions,
+/// find the partition that varies the most in RGB width, and return its index.
+fn findPartitionToSplit(partitions: []ColorSpace) ?usize {
+    var max_size = 0;
+    var split_index = 0;
+    var found = false;
+    for (0..partitions.len) |i| {
+        const partition = partitions[i];
+        if (max_size < partition.rgb_width and partition.num_colors > 1) {
+            max_size = partition.rgb_width;
+            split_index = i;
+            found = true;
+        }
+    }
+    if (!found) return null;
+    return split_index;
+}
+
+/// Recursively split the colorspace into smaller partitions until 2**depth partitions are created.
+fn medianCut(allocator: std.mem.Allocator, first_partition: *ColorSpace, depth: u3) ![]*ColorSpace {
     const total_partitions = 2 ** depth;
 
     var parts = try allocator.alloc(*ColorSpace, total_partitions);
     parts[0] = first_partition;
 
     var n_partitions = 1; // we're starting with 1 large partition.
-    while (n_partitions != total_partitions) : (n_partitions += 1) {
+    while (n_partitions < total_partitions) : (n_partitions += 1) {
         // Look for the partition that has the largest variance in RGB width.
-        // Then split that into two halves.
-        var max_size = 0;
-        var split_index = parts.len + 1;
-        var found = false;
-        for (0..n_partitions) |i| {
-            const partition = parts[i];
-            if (max_size < partition.rgb_width and partition.num_colors > 1) {
-                max_size = partition.rgb_width;
-                split_index = i;
-                found = true;
-            }
-        }
-
-        if (!found) {
-            break;
-        }
+        const split_index_ = findPartitionToSplit(parts[0..n_partitions]);
+        const split_index = if (split_index_) |index| index else break;
 
         // We found the partition that varies the most in either of the 3 color channels.
-        const partition = parts[split_index];
-        const sort_channel = partition.widest_channel;
-
-        // Create an array with all the colors in the partition, then sort it by the widest channel.
-        var sorted_colors = try allocator.alloc(*QuantizedColor, partition.num_colors);
+        const partition_to_split = parts[split_index];
+        // sort the colors in that partition along the widest channel.
+        const sorted_colors = try sortPartition(allocator, partition_to_split);
         defer allocator.free(sorted_colors);
-
-        // Copy all the colors in this partition into the array for sorting.
-        var color = partition.colors;
-        var j = 0;
-        while (color != null and j < sorted_colors.len) : (color = color.next) {
-            sorted_colors[j] = &color;
-            j += 1;
-        }
-
-        // Sort the colors!
-        std.sort.heap(
-            *QuantizedColor,
-            sorted_colors,
-            sort_channel,
-            colorLessThan,
-        );
-
-        // Re-link the sorted colors in their new order.
-        for (0..sorted_colors.len - 1) |i| {
-            sorted_colors[i].next = sorted_colors[i + 1];
-        }
-        sorted_colors[sorted_colors.len - 1].next = null;
-
-        // The first half of the sorted array will remain in the current partition.
-        partition.colors = sorted_colors[0];
+        // Now the colors in the partition are sorted along the widest channel.
+        partition_to_split.colors = sorted_colors[0];
 
         // Create a new partition. We will populate this struct below.
         const new_partition = try allocator.create(ColorSpace);
-        parts[n_partitions] = new_partition;
 
         // Next, we find the *median* color in the sorted list of colors.
         // NOTE: The median is NOT the middle element.
         // Rather, its the element that divides the array such that both halves
         // have roughly the same pixel-frequency.
         // AKA sum([color.frequency for color in left]) =  sum(color.frequency for color in right).
-        color = partition.colors;
-        const target_pixel_count = partition.num_pixels / 2;
-        var remaining_pixel_count = 0; // # of pixels that will remain in the current partition.
-        var remaining_color_count = 0; // # of colors that will remain in the current partition.
-        while (color.next != null) : (color = color.next) {
+        var color = partition_to_split.colors;
+
+        // # of pixels that should remain in the current partition (left half).
+        // The first color in the current partition will remain in it.
+        const target_pixel_count = partition_to_split.num_pixels / 2;
+        var remaining_pixel_count = color.frequency; // # of pixels that will remain in the current partition.
+        var remaining_color_count = 1; // # of colors that will remain in the current partition.
+
+        // At the end of this loop, `color` will point to the last color that will remain
+        // in the current partition, and `color.next` will be the first color in the new partition.
+        while (true) {
+            // Stop if we are on the second last item in the linked list.
+            const next_color = if (color.next) |c| c else break;
+            if (next_color.next == null) {
+                break;
+            }
+
+            color = next_color;
             remaining_color_count += 1;
-            remaining_pixel_count += color.frequency;
+            remaining_pixel_count += next_color.frequency;
+
+            // We've reached the target pixel freuqency for the left half (remaining parition).
             if (remaining_pixel_count >= target_pixel_count) break;
         }
 
-        // ounding box for the new partition.
-        new_partition.rgb_min = partition.rgb_max;
+        // At this point, `color` is the last color in the left partition.
+        const last_of_left = color;
+        const first_of_right = if (last_of_left.next) |c| c else unreachable;
 
-        // Set up the new partition
-        new_partition.colors = color.next;
-        new_partition.num_pixels = partition.num_pixels - remaining_pixel_count;
-        new_partition.num_colors = partition.num_colors - remaining_color_count;
+        // Set up the new partition (right half).
+        new_partition.colors = first_of_right;
+        last_of_left.next = null; // unlink the two partitions.
 
-        partition.num_colors = remaining_color_count;
-        partition.num_pixels = remaining_pixel_count;
+        new_partition.num_pixels = partition_to_split.num_pixels - remaining_pixel_count;
+        new_partition.num_colors = partition_to_split.num_colors - remaining_color_count;
 
-        color.next = null;
+        partition_to_split.num_colors = remaining_color_count;
+        partition_to_split.num_pixels = remaining_pixel_count;
 
-        std.debug.assert(partition.num_pixels == countPixels(partition, partition.num_colors));
+        // Update the bounding boxes of the two partitions.
+        // TODO: can I optimize this somehow? GIFLIB seems to use some kind of a trick here.
+        findWidestChannel(partition_to_split);
+        findWidestChannel(new_partition);
+
+        // add the new partition to the partitions array.
+        parts[n_partitions] = new_partition;
+
+        std.debug.assert(partition_to_split.num_pixels == countPixels(partition_to_split, partition_to_split.num_colors));
         std.debug.assert(new_partition.num_pixels == countPixels(new_partition, new_partition.num_colors));
-
-        // Update the bounding box of the current partition.
     }
+
+    std.debug.assert(n_partitions == total_partitions);
+    return parts;
 }
 
+/// Returns the sum of frequencies of all colors in the partition.
 fn countPixels(partition: *ColorSpace, ncolors: usize) usize {
     var count = 0;
     var color = partition.colors;
